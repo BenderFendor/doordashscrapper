09568bc027db57c086e9650ab5fe2cacbfa9a664
Bender <bender@DESKTOP-698PA65>
2024-04-29 01:05:16 -0400

    Removed changed.py

--- change.py
+++
@@ -1,5 +0,0 @@
-from gensim.scripts.glove2word2vec import glove2word2vec
-
-glove_input_file = 'glove.42B.300d.txt'
-word2vec_output_file = 'glove.42B.300d.word2vec.txt'
-glove2word2vec(glove_input_file, word2vec_output_file)
